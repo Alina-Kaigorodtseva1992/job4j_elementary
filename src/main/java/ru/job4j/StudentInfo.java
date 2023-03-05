07f4f5b8b4f2e27a3a6980c27b69492ceab950af
Алина <alinabeljalova@gmail.com>
2023-03-06 06:52:05 +0700

    Проведена проверка CheckStyle, были лишние пробелы перед скобками

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main( String[] args) {
+    public static void main(String[] args) {
         System.out.println("Alina Kaigorodtseva");
         System.out.println("05.09.1992");
 
