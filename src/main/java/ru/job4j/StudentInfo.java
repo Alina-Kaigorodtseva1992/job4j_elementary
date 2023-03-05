66d7dc6fb2d2fded4e3e873feaa7438bc06d343b
Алина <alinabeljalova@gmail.com>
2023-03-06 06:25:34 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main( String[] args) {
+        System.out.println("Alina Kaigorodtseva");
+        System.out.println("05.09.1992");
+
+    }
+}
